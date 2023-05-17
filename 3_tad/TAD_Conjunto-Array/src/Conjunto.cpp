dd51ee37d961764c000586b7294c160d39ee7625
Iago Macedo <iago.macedo.139@ufrn.edu.br>
2023-05-17 02:03:13 -0300

    primeiras alterações ativ

--- 3_tad/TAD_Conjunto-Array/src/Conjunto.cpp
+++ 3_tad/TAD_Conjunto-Array/src/Conjunto.cpp
@@ -3,42 +3,63 @@
 
 Conjunto::Conjunto(int capacidade)
 {
-    //TO-DO
+    this->quantidade=0;
+    this->capacidade=capacidade;
+    this->elementos= new int[capacidade];
 }
 
 Conjunto::~Conjunto()
 {
-    //TO-DO
+    delete []this->elementos;
 }
 
 bool Conjunto::buscar(int elemento) const
 {
-   //TO-DO
-    return elemento != 0;
+    for(int i =0;i<this->quantidade;i++){
+        if(elemento==*(this->elementos+i)){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Conjunto::inserir(int elemento)
 {
-    //TO-DO
+    if(this->quantidade<this->capacidade && !this->buscar(elemento)){
+        this->elementos[this->capacidade] = elemento;
+        this->quantidade++;
+    } else{
+        return false;
+    }
     return elemento != 0;
 }
 
 bool Conjunto::remover(int elemento)
 {
-    //TO-DO
-    return elemento != 0;
+    int indiceSwap;
+    if(buscar(elemento)){
+        for(int i =0;i<this->quantidade;i++){
+            if(this->elementos[i]==elemento){
+                indiceSwap = i;
+                break;
+            }
+        }
+        this->elementos[indiceSwap] = this->elementos[this->quantidade-1];
+        this->quantidade--;
+        return true;
+    }
+    return false;
 }
 
 int Conjunto::tamanho() const
 {
-    //TO-DO
-    return 0;
+    return this->quantidade;
 }
 
 void Conjunto::imprimir() const
 {
     std::cout << "{ ";
-    for(int i = 0; i < quantidade; ++i)
+    for(int i = 0; i < this->quantidade; ++i)
     {
         std::cout << this->elementos[i] << " ";
     }
